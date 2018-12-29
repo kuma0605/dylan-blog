80f6cf08c0cfa12fb4ad0184ff81de29f62ac035
dylan <825880445@qq.com>
2018-12-29 19:04:13 +0800

    amend

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 import { rhythm, scale } from '../utils/typography'
 
+import BackgroundImage from 'gatsby-background-image'
+
+import layoutStyle from './layout.module.css'
+
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
+    const { location, title, children , data} = this.props
+    
     const rootPath = `${__PATH_PREFIX__}/`
     let header
 
@@ -52,13 +57,22 @@ class Layout extends React.Component {
         </h3>
       )
     }
+    const imageData = data.wallPaper.childImageSharp.fluid
+    console.log(imageData)
     return (
+      <BackgroundImage 
+        className={layoutStyle.container}
+        fluid={imageData}>
       <div
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          // maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(8 / 4)}`,
+          position: "relative",
+          
+          width:`100%`,
+          height:`100%`
         }}
       >
         {header}
@@ -67,8 +81,32 @@ class Layout extends React.Component {
           © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
+      </BackgroundImage> 
     )
   }
 }
 
-export default Layout
+// export default Layout
+const layoutQuery = graphql`
+ query {
+  wallPaper:file(relativePath: { eq: "wallPaper.jpg" }){
+    childImageSharp{
+      fluid (quality: 100, maxWidth: 4160){
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+ }
+`
+export default props => {
+  return (<StaticQuery 
+    query={layoutQuery}
+    render={ data => {
+        return (<Layout data={data} {...props} />) 
+      }
+    }
+  >
+  </StaticQuery>)
+}
+
+
